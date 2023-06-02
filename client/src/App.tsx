712c6c7382ab18c15feb1a3ee08d4a07d1cf2ffe
Bryson Monteiro <brysonmonteiro@brysons-air.mynetworksettings.com>
2023-06-01 21:12:49 -0400

    deleted app.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime doloribus molestias, id quaerat optio nisi at laborum. Voluptate rerum id quibusdam consequatur sint, autem eligendi ipsam quia assumenda ducimus.</div>
+      <div>PAULO IS A MONKEY.</div>
     </div>
   );
 }
